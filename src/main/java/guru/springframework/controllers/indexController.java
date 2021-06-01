2891a67aaac7d6eb6e5f58aa23dc162c68ce75dd
anookinov <anookinov@gmail.com>
2021-06-01 16:53:03 +0700

    add index controller and recipe and notes domain models

---
+++ src/main/java/guru/springframework/controllers/indexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index"})
+    public String getIndexPage() {
+        System.out.println("Some message to say... 123");
+        return "index";
+    }
+}
